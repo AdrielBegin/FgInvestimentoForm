6a624b94e73dd3fae475599f97d780c02e796e53
AdrielBegin <adriel.pemi@gmail.com>
2025-04-21 06:02:18 -0300

    feat: ajusta css

--- app/consulta/page.tsx
+++ app/consulta/page.tsx
@@ -5,7 +5,7 @@ import ConsultaAlunos from '@/app/components/consultarAluno/Alunos';
 
 export default function GetAlunos() {
   return (
-    <div className="min-h-screen flex items-center justify-center">
+    <div className="">
       <ConsultaAlunos />
     </div>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@
 import HomePage from '@/app/components/paginaInicial/HomePag';
 export default function InicialPage() {
   return (
-    <div className="min-h-screen flex items-center justify-center">
+    <div className="">
       <HomePage />
     </div>
   );
